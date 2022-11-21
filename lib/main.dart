c119d28ab002ec7e7cadd0c859b922420317d691
anubhavmishra7 <anubhavcv77@gmail..com>
2022-11-21 16:16:55 +0530

    commit2

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'KEBS',
+      title: 'KEBBS',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
